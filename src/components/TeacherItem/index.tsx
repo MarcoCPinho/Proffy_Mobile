85f34dfbf1d04c38b7249c68f660b9765c203824
MarcoCPinho <marcocpinho@hotmail.com>
2020-08-08 11:05:54 -0300

    finishing the basic

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,53 +1,118 @@
-import React from 'react';
-import { View, Image, Text } from 'react-native';
-
-import PageHeader from '../PageHeader';
-import styles from './styles';
+import React, { useState } from 'react';
+import { View, Image, Text, Linking } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import api from '../../services/api';
+
 import heartOutlineeIcon from '../../assets/images/icons/heart-outline.png'
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png'
 import whatsappIcon from '../../assets/images/icons/whatsapp.png'
 
-function TeacherItem() {
+import styles from './styles';
+
+export interface Teacher {
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+    favorited: boolean;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
+    const [isFavorited, setIsFavorited] = useState(favorited);
+
+    function handleLinkToWhatsapp() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+
+        Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`)
+    }
+
+    async function handleToggleFavorite() {
+        const favorites = await AsyncStorage.getItem('favorites'); //cria a constante "favorites", recebendo do "storage do local do async" (AsyncStorage), os valores (getItem), que receberam o nome de "favorites"
+
+        let favoritesArray = [];
+
+        if (favorites) {
+            favoritesArray = JSON.parse(favorites); //favorites array recebe o json transformado de string para int
+        }
+
+        if (isFavorited) {
+            //remover dos favoritos
+            const favoriteIndex = favoritesArray.findIndex((teacherItem: Teacher) => {
+                return teacherItem.id == teacher.id;
+            })
+
+            favoritesArray.splice(favoriteIndex, 1);
+
+            setIsFavorited(false);
+        } else {
+            //adicionar aos favoritos
+
+            favoritesArray.push(teacher);
+
+            setIsFavorited(true);
+        }
+
+        await AsyncStorage.setItem('favorites', JSON.stringify(favoritesArray)) //"favorites" recebe um JSON, transformado para string, tendo como dados o "favoritesAArray" (que eu transformei em INT no inicio)
+
+    };
+
     return (
-    <View style={styles.container}>
-       <View style={styles.profile} >
-            <Image 
-            style={styles.avatar}
-            source={{ uri: 'https://avatars0.githubusercontent.com/u/68476808?s=460&u=e46db5c112259f8f91aebb0a9732b6e492e9af59&v=4'}}
-            />
+        <View style={styles.container}>
+            <View style={styles.profile} >
+                <Image
+                    style={styles.avatar}
+                    source={{ uri: teacher.avatar }}
+                />
 
-            <View style={styles.profileInfo}>
-                <Text style={styles.name}>Marco Carvalho de Pinho</Text>
-                <Text style={styles.subject}>Química</Text>
+                <View style={styles.profileInfo}>
+                    <Text style={styles.name}>{teacher.name}</Text>
+                    <Text style={styles.subject}>{teacher.subject}</Text>
+                </View>
             </View>
-       </View>
 
-       <Text style={styles.bio}>
-            Aprendendo a desenvolver em React Native.
-            {'\n'}{'\n'}
-            Este APP já encontra-se disponível na web.
-       </Text>
+            <Text style={styles.bio}>
+                {teacher.bio}
+            </Text>
 
-       <View style={styles.footer}>
-           <Text style={styles.price}>
-               Preço/hora {'   '}
-               <Text style={styles.priceValue}>R$ 20,00</Text>
-           </Text>
+            <View style={styles.footer}>
+                <Text style={styles.price}>
+                    Preço/hora {'   '}
+                    <Text style={styles.priceValue}>R$ {teacher.cost}</Text>
+                </Text>
 
-           <View style={styles.buttonsContainer}>
-               <RectButton style={[styles.favoriteButton, styles.favorited]}>
-                   {/*<Image source={heartOutlineeIcon}/>*/}
-                   <Image source={unfavoriteIcon}/>
-               </RectButton>
+                <View style={styles.buttonsContainer}>
+                    <RectButton
+                        onPress={handleToggleFavorite}
+                        style={[
+                            styles.favoriteButton,
+                            isFavorited ? styles.favorited : {},
+                        ]}
+                    >
+                        {isFavorited
+                            ? <Image source={unfavoriteIcon} />
+                            : <Image source={heartOutlineeIcon} />
+                        }
 
-               <RectButton style={styles.contactButton}>
-                   <Image source={whatsappIcon}/>
-                   <Text style={styles.contactButtonText}>Entrar em contato</Text>
-               </RectButton>
-           </View>
-       </View>
-    </View>
+                    </RectButton>
+
+                    <RectButton onPress={handleLinkToWhatsapp} style={styles.contactButton}>
+                        <Image source={whatsappIcon} />
+                        <Text style={styles.contactButtonText}>Entrar em contato</Text>
+                    </RectButton>
+                </View>
+            </View>
+        </View>
     )
 }
 
--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-community/async-storage';
+import { useFocusEffect } from '@react-navigation/native'
+
 
 import styles from './styles';
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+
 
 function Favorites (){
+    const [favorites, setFavorites] = useState([]);
+
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then(response => {
+            if (response) {
+                const favoritedTeachers = JSON.parse(response);
+
+                setFavorites(favoritedTeachers);
+            }
+        });
+    }
+
+    useFocusEffect(() => {
+        loadFavorites();
+    });
+    
     return (
         <View style={styles.container}>
             <PageHeader title="Meus proffys favoritos"/>
@@ -18,11 +38,15 @@ function Favorites (){
                 paddingBottom: 16,
             }}
             >
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+                {favorites.map((teacher: Teacher) => {
+                    return (
+                        <TeacherItem
+                        key={teacher.id}
+                        teacher={teacher}
+                        favorited={true} //se eu fizer somente "favorited", o react vai automaticamente entender que estou colocando "true"
+                        />
+                    )
+                })}
             </ScrollView>
 
         </View>
